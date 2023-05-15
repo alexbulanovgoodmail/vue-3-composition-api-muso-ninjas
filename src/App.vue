122734995b3309238b463441a86839947de248be
Alexander Bulanov <alex-bulanov.mail@yandex.ru>
2023-05-15 20:04:57 +0300

    Preview & Setup

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="content">
+    <RouterView />
+  </div>
+</template>
+
+<style lang="scss">
+.content {
+  margin: 0 auto;
+  max-width: 1200px;
+  width: 100%;
+  padding: 0 20px;
+}
+</style>
